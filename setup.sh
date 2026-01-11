ef4ec47d10bb2dd0be31f3c2dfbf6a708324f76d
Leandro Pereira <leandropantoja@gmail.com>
2026-01-11 17:26:09 -0300

    initial

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
   QUESTÃO 8
 
-  oc new-project turquoise
+oc new-project turquoise
 
-  oc new-project nexus
+oc new-project nexus
 
-  oc create -f nexus3-persistent-template.yaml
+oc create -f nexus3-persistent-template.yaml
 
-  oc new-app nexus3-persistent
+oc new-app nexus3-persistent
 
-  #oc new-app nexus3-persistent -p NEXUS_VERSION=3.87.1
+#oc new-app nexus3-persistent -p NEXUS_VERSION=3.87.1
 
 
-  configurar webhohk no repo:
+configurar webhohk no repo:
 
-  https://github.com/leandroppereira/employee-backend.git
+https://github.com/leandroppereira/employee-backend.git
 
 curl -H "Authorization: Bearer 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" \
      https://api.github.com/repos/leandroppereira/employee-backend/hooks
@@ -29,5 +29,5 @@ curl -X PATCH \
     }
   }'
 
-  expor eventListener
-  oc create route edge --service=el-employee-backend --insecure-policy='Allow'
+expor eventListener
+oc create route edge --service=el-employee-backend --insecure-policy='Allow'
